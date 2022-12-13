35ca47049bd0b1e944c73fe9ca2ea477f3780255
GabrScarpinella <118476744+GabrScarpinella@users.noreply.github.com>
2022-12-13 13:52:14 -0300

    Update HeartbeatProject.ino

--- HeartbeatProject.ino
+++ HeartbeatProject.ino
@@ -11,8 +11,7 @@ int buzzer=8;
 int bpm;
 
 PulseSensorPlayground pulseSensor;
-Adafruit_NeoPixel faixa(ledes, pin , NEO_GRB + NEO_KHZ800);
-
+Adafruit_NeoPixel faixa(ledes, pin, NEO_GRB+NEO_KHZ800);
 
 void setup(){
   faixa.begin();
@@ -21,7 +20,6 @@ void setup(){
   pulseSensor.begin();
   pinMode(buzzer,OUTPUT);
 }
-
 
 void loop(){
   bpm = pulseSensor.getBeatsPerMinute();
@@ -36,25 +34,25 @@ void loop(){
 
 void faixaLiga(){
   tone(buzzer,256, 100);
-  for (int i = 0; i < ledes; i++){
+  for (int i=0; i<ledes; i++){
     faixa.setPixelColor(i, faixa.Color(255, 0, 0));
     faixa.show();
-    delay(1500 / bpm);
+    delay(1500/bpm);
   }
   noTone(buzzer);
 }
 void faixaDesliga(){
-  for (int i = ledes; i > 0; i--){
+  for (int i=ledes; i>0; i--){
     faixa.setPixelColor(i, faixa.Color(255, 255, 255));
     faixa.show();
-    delay(1500 / bpm);
+    delay(1500/bpm);
   }
 }
 void morreu(){
   tone(buzzer,256);
   faixa.setPixelColor(0, faixa.Color(255, 0, 0));
   faixa.show();
-  for (int i = 0; i < 256; i++){
+  for (int i=0; i<256; i++){
     faixa.setPixelColor(0, faixa.Color(255, i, i));
     faixa.show();
     delay(5);
